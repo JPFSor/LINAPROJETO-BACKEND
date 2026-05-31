4abaf32005e57f86256afe59e588963404c99b89
JPFSor <jpfsor06@gmail.com>
2026-05-31 15:32:13 -0300

    Create V6__atualizar_avaliacao_nutricionistas.sql

---
+++ lina/src/main/resources/db/migration/V6__atualizar_avaliacao_nutricionistas.sql
@@ -0,0 +1,6 @@
+-- V6: Atualizar avaliações dos nutricionistas (escala 1 a 5)
+UPDATE nutricionista SET avaliacao = 1 WHERE id = 1;
+UPDATE nutricionista SET avaliacao = 2 WHERE id = 2;
+UPDATE nutricionista SET avaliacao = 3 WHERE id = 3;
+UPDATE nutricionista SET avaliacao = 4 WHERE id = 4;
+UPDATE nutricionista SET avaliacao = 5 WHERE id = 5;
